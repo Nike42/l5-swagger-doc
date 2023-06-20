7655f27d713b194d7753c567fb949318704a1149
Lawal Mary N <95449475+Nike42@users.noreply.github.com>
2023-06-20 00:20:15 -0700

    init

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
- use App\Http\Controllers\SellerController;
+use App\Http\Controllers\SellerController;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\Api\Api\AuthController;
 /*
+
+
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
@@ -16,23 +19,34 @@ use App\Http\Controllers\ProductsController;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 }) ;
 
- 
+#Route::get('/', function () {
+#   return response()->json([
+ #     'message' => 'Welcome to the laravel API'
+  #]);
+#});
+
+   Route::post('login', 'Api\AuthController@login');
+   Route::post('register', 'Api\AuthController@register');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+ return $request->user();
+});
 
 Route::group(["prefix"=>"seller"],function(){
-    Route::get("/get/{id}",[SellerController::class,"get"]);
-    Route::get("/gets",[SellerController::class,"gets"]);
-    Route::post("/store",[SellerController::class,"store"]);
-    Route::put("/update/{id}",[SellerController::class,"update"]);
-    Route::delete("/delete/{id}",[SellerController::class,"delete"]);
+  Route::get("/get/{id}",[SellerController::class,"get"]);
+  Route::get("/gets",[SellerController::class,"gets"]);
+  Route::post("/store",[SellerController::class,"store"]);
+  Route::put("/update/{id}",[SellerController::class,"update"]);
+  Route::delete("/delete/{id}",[SellerController::class,"delete"]);
 });
 
 Route::group(["prefix"=>"products"],function(){
-  Route::get("/get/{id}",[ProductsController::class,"get"]);
-  Route::get("/gets",[ProductsController::class,"gets"]);
-  Route::post("/store",[ProductsController::class,"store"]);
-  Route::put("/update/{id}",[ProductsController::class,"update"]);
-  Route::delete("/delete/{id}",[ProductsController::class,"delete"]);
+Route::get("/get/{id}",[ProductsController::class,"get"]);
+Route::get("/gets",[ProductsController::class,"gets"]);
+Route::post("/store",[ProductsController::class,"store"]);
+Route::put("/update/{id}",[ProductsController::class,"update"]);
+Route::delete("/delete/{id}",[ProductsController::class,"delete"]);
 });
